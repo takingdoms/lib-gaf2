6bda33d215aa053964e2a209464b51c130e34a33
C L <c1000@protonmail.com>
2024-02-24 19:14:37 -0300

    barrel pattern for exporting

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-export * from './color';
-export * from './gaf-types';
-
-export * as ColorUtils from './color-utils';
-export * as Reader from './gaf-reader';
-export * as Writer from './gaf-writer';
-export * as Internals from './internals';
+import * as LibGaf from './barrel';
+export default LibGaf;
