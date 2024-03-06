a47ab55b08cda1d61f66475b5310ab8e6af4c1e4
C L <c1000@protonmail.com>
2024-03-06 18:42:37 -0300

    implemented the rgba8888 to argb4444 conversion

--- src/color-utils/data-converters/rgba8888-to-argb4444.ts
+++ src/color-utils/data-converters/rgba8888-to-argb4444.ts
@@ -16,7 +16,30 @@ export const convertRGBA8888ToARGB4444: DataConverter<'rgba8888', 'argb4444', Op
 
   const convert8to4 = options.depthConvert8to4 ?? DepthConverters.round8to4;
 
-  throw 'TODO';
+  for (let i = 0; i < imgWidth * imgHeight; i++) {
+    const inputOffset = i * 4;  // 4 = 4 bytes per pixel
+    const outputOffset = i * 2; // 2 = 2 bytes per pixel
+
+    const inputRed   = input.bytes[inputOffset + 0];
+    const inputGreen = input.bytes[inputOffset + 1];
+    const inputBlue  = input.bytes[inputOffset + 2];
+    const inputAlpha = input.bytes[inputOffset + 3];
+
+    const outputRed   = convert8to4(inputRed);
+    const outputGreen = convert8to4(inputGreen);
+    const outputBlue  = convert8to4(inputBlue);
+    const outputAlpha = convert8to4(inputAlpha);
+
+    const outputUint16 = (outputAlpha << 12) | (outputRed << 8) | (outputGreen << 4) | outputBlue;
+
+    outputBytes[outputOffset + 0] = outputUint16 & 0xFF;
+    outputBytes[outputOffset + 1] = (outputUint16 >> 8) & 0xFF;
+  }
+
+  return {
+    format: 'argb4444',
+    bytes: outputBytes,
+  };
 };
 
 export { Options as OptionsRGBA8888ToARGB4444 };
